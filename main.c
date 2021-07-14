ce1a66db5ff78b1b31d0c5cd1e95bc667ebf5c33
berkaymhc <71499075+berkaymhc@users.noreply.github.com>
2021-07-14 13:29:19 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 
 int main()
 {
+    char name[80];
     FILE *belge;
-    char karakter;
-    belge=fopen("C:\\Users\\berka\\OneDrive\\Masaüstü\\belge.txt.txt","r");
-    do{
+    char word[500];
 
-    karakter=getc(belge);
 
-    printf("%c",karakter);
+    belge = fopen("C:\\Users\\berka\\OneDrive\\Masaüstü\\belge.txt", "r");
+
+    if (belge == NULL)
+        printf("Can't open %s for reading.\n", name);
+    else
+    {
+        while (fscanf(belge, "%s", word) != EOF)
+        {
+            printf("%s\n", word);
+        }
+        fclose(belge);
     }
-    while(karakter!=EOF);
-    fclose(belge);
-
-
     return 0;
 }
-
-
-
-
